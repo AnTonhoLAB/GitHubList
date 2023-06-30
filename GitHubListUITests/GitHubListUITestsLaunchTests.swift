df9caa9f47e926d9857cd40b8bf1e03d994a1984
George Gomes <georgevag@protonmail.com>
2023-06-29 22:31:44 -0300

    Add project

---
+++ GitHubListUITests/GitHubListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubListUITestsLaunchTests.swift
+//  GitHubListUITests
+//
+//  Created by George Gomes on 29/06/23.
+//
+
+import XCTest
+
+final class GitHubListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
