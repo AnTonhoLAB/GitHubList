b7c1ee021f4af72732e003484dc39ea1d1e5ce91
George Gomes <georgevag@protonmail.com>
2023-07-02 11:05:05 -0300

    Add notFound error

--- GitHubList/MainService/RequesterProtocol.swift
+++ GitHubList/MainService/RequesterProtocol.swift
@@ -15,10 +15,20 @@ extension RequesterProtocol {
     func makeRequest<T>(url: String, urlParams: [String: String] = [:], single: @escaping (Result<T, Error>) -> Void) where T: Decodable {
         AF.request(url, parameters: urlParams)
             .responseDecodable(of: T.self) { (response) in
+
                 switch response.result {
                 case .success(let result):
                     single(.success(result))
                 case .failure(let error):
+                    guard let statusCode = response.response?.statusCode else {
+                        single(.failure(error))
+                        return
+                    }
+                    
+                    if statusCode == 404 {
+                        single(.failure(GitHubServiceError.notFound))
+                    }
+                    
                     single(.failure(error))
                 }
         }
